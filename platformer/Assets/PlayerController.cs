cc4d941b38b0cb5118757b35426fd4196298eae0
vad710 <vinny.dasilva@gmail.com>
2014-02-22 18:15:05 -0500

    Fixing some issues related to Jumping

--- platformer/Assets/PlayerController.cs
+++ platformer/Assets/PlayerController.cs
@@ -6,30 +6,68 @@ namespace Assets
     {
         private const string HorizontalAxis = "Horizontal";
         private const string IsMovingAnimationParameter = "IsMoving";
+        private const string IsJumpingAnimationParameter = "IsJumping";
+        private const string IsLandingAnimationParameter = "IsLanding";
 
         private const int MovingForward = 1;
         private const int MovingBackward = 0;
+        private const float MovementSpeed = 0.1f;
+        private const int JumpForce = 250;
 
         private Animator _animator;
         private Transform _transform;
+        private Rigidbody2D _rigidBody;
+        private int _groundLayer;
+
 
         private int _previousDirection = 1;
         private bool _runningAtFullSpeed;
+        private bool _grounded = true;
+        private bool _isJumping = false;
 
 
         // Use this for initialization
         public void Start ()
         {
             _animator = this.GetComponent<Animator>();
-            _transform = this.GetComponent<Transform>();
+            _transform = this.transform;
+            _rigidBody = this.rigidbody2D;
+
+            _groundLayer = LayerMask.NameToLayer("Ground");
         }
-	
+
+
+        private void CheckLanding()
+        {
+            //Determines when the player begins to fall after he begins jumping
+
+            if (_isJumping)
+            {
+                if (_rigidBody.velocity.y <= 0.75)
+                {
+                    _animator.SetBool(IsJumpingAnimationParameter, false);
+                    _animator.SetBool(IsLandingAnimationParameter, true);
+                }
+            }
+        }
+
+
         // Update is called once per frame
         public void Update ()
         {
 
+            //this.CheckLanding();
+
             //var vertical = Input.GetAxis("Vertical");
-            
+
+            if (Input.GetButtonDown("Jump") && _grounded)
+            {
+                rigidbody2D.AddForce(new Vector2(0f, JumpForce));
+                _animator.SetBool(IsJumpingAnimationParameter, true);
+                _isJumping = true;
+                _grounded = false;
+            }
+
             
             int direction;
             bool isMoving;
@@ -63,9 +101,21 @@ namespace Assets
 
             //Update movement
             var position = _transform.position;
-            position.x += (horizontal * 0.1f);
+            position.x += (horizontal * MovementSpeed);
             _transform.position = position;
 
+        }
+
+        public void OnCollisionEnter2D(Collision2D collision) 
+        {
+            //Debug.Log("OnCollisionEnter!");
+
+            if (!_grounded && _isJumping && collision.gameObject.layer == _groundLayer)
+            {
+                _grounded = true;
+                _isJumping = false;
+                _animator.SetBool(IsJumpingAnimationParameter, false);
+            }
         }
 
         private float GetHorizontalMovement()
