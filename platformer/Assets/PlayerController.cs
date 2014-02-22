43e5d267ae0cd53fac19046fe1e9a804ddeae72b
vad710 <vinny.dasilva@gmail.com>
2014-02-22 12:49:07 -0500

    Adding Animations

---
+++ platformer/Assets/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private const string HorizontalAxis = "Horizontal";
+        private const string DirectionAnimationParameter = "Direction";
+        private const string IsMovingAnimationParameter = "IsMoving";
+
+
+        private Animator _animator;
+
+        // Use this for initialization
+        public void Start ()
+        {
+            _animator = this.GetComponent<Animator>();
+        }
+	
+        // Update is called once per frame
+        public void Update ()
+        {
+            //var vertical = Input.GetAxis("Vertical");
+            var horizontal = Input.GetAxis(HorizontalAxis);
+
+            Debug.Log(horizontal);
+
+            if (horizontal > 0)
+            {
+                _animator.SetInteger(DirectionAnimationParameter, 1);
+                _animator.SetBool(IsMovingAnimationParameter, true);
+            }
+            else if (horizontal < 0)
+            {
+                _animator.SetInteger(DirectionAnimationParameter, 0);
+                _animator.SetBool(IsMovingAnimationParameter, true);
+            }
+            else
+            {
+                _animator.SetBool(IsMovingAnimationParameter, false);
+            }
+
+
+        }
+    }
+}
