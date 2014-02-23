7d0a90e5c558a000a5ed8cb04b8ead93a36d62b5
vad710 <vinny.dasilva@gmail.com>
2014-02-23 15:19:40 -0500

    Added Second General Dialog

---
+++ platformer/Assets/SpawnOnTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    public class SpawnOnTrigger : MonoBehaviour
+    {
+        public Transform PrefabToSpawn;
+        public Transform TriggerObject;
+        public Vector3 SpawnPosition;
+
+        private bool _triggered;
+
+        public void OnTriggerEnter2D(Collider2D other)
+        {
+            if (PrefabToSpawn == null || _triggered)
+            {
+                return;
+            }
+
+            if (other == TriggerObject.collider2D)
+            {
+                Instantiate(PrefabToSpawn, SpawnPosition, Quaternion.Euler(Vector3.zero));
+                _triggered = true;
+            }
+        }
+    }
+}
