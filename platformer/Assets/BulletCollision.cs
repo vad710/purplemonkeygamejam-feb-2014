1d85ab16dfa1902086e45772a6a79ce66cce0308
vad710 <vinny.dasilva@gmail.com>
2014-02-23 00:39:49 -0500

    Adding Bullet Prefab

---
+++ platformer/Assets/BulletCollision.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    public class BulletCollision : MonoBehaviour
+    {
+
+        public AudioClip HitPersonAudioClip;
+        public AudioClip RicochetAudioClip;
+
+
+
+        //// Use this for initialization
+        //void Start () 
+        //{
+	
+        //}
+	
+        //// Update is called once per frame
+        //void Update () 
+        //{
+	
+        //}
+
+        public void OnCollisionEnter2D(Collision2D collision)
+        {
+
+            //determine if the bullet is hitting a person or a thing
+            this.audio.clip = this.HitPersonAudioClip;
+            this.audio.Play();
+        }
+    }
+}
--- platformer/Assets/Shoot.cs
+++ platformer/Assets/Shoot.cs
@@ -1,23 +1,67 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace Assets
 {
     public class Shoot : MonoBehaviour
     {
 
+        public Rigidbody2D Bullet;
+
+        private const float RateOfFire = 1f;
+        private const float BulletSpeed = 10;
+
         private PlayerController _playerController;
+        private float _nextFireTime;
+
 
 
         // Use this for initialization
-        void Start () 
+        public void Start () 
         {
             _playerController = transform.root.GetComponent<PlayerController>();
+            _nextFireTime = Time.time;
         }
 	
         // Update is called once per frame
-        void Update () 
+        public void Update () 
         {
-	        
+            // If the fire button is pressed...
+            if (Input.GetButton("Fire1"))
+            {
+                this.Fire();
+            }	        
+        }
+
+        private void Fire()
+        {
+
+            if (Time.time >= _nextFireTime)
+            {
+
+                Debug.Log("Fire!!");
+
+                // play the audioclip.
+                audio.Play();
+
+                //// If the player is facing right...
+                if (_playerController.IsFacingRight)
+                {
+                    // ... instantiate the rocket facing right and set it's velocity to the right. 
+                    var bulletInstance = Instantiate(Bullet, transform.position, Quaternion.Euler(new Vector3(0, 0, 0))) as Rigidbody2D;
+                    bulletInstance.velocity = new Vector2(BulletSpeed, 0);
+                }
+                else
+                {
+                //    // Otherwise instantiate the rocket facing left and set it's velocity to the left.
+                    var bulletInstance = Instantiate(Bullet, transform.position, Quaternion.Euler(new Vector3(0, 0, 180f))) as Rigidbody2D;
+                    bulletInstance.velocity = new Vector2(-BulletSpeed, 0);
+                }
+
+                _nextFireTime = Time.time + RateOfFire;
+            }
+
+            
         }
     }
 }
