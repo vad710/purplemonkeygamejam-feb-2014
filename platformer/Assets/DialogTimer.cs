3a6ece01e496cacb9dbc2395ec2c5670fa9745ee
vad710 <vinny.dasilva@gmail.com>
2014-02-23 15:06:28 -0500

    Added initial Dialog

---
+++ platformer/Assets/DialogTimer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    public class DialogTimer : MonoBehaviour
+    {
+
+        public Transform NextDialog;
+
+        private const float DialogTime = 6f;
+        private float _finishTime = 0f;
+
+        // Use this for initialization
+        public void Start ()
+        {
+            _finishTime = Time.time + DialogTime;
+        }
+	
+        // Update is called once per frame
+        public void Update () 
+        {
+            if (Time.time >= _finishTime)
+            {
+                if (NextDialog != null)
+                {
+                    Instantiate(NextDialog, transform.position, Quaternion.Euler(Vector3.zero));
+                }
+
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
